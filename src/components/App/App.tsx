d4202e4c4eae1ed87ca216890fbe9ba87a38d1f2
unknown <cristianoalmeida.souza@gmail.com>
2020-12-08 15:05:27 -0600

    disable class component useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from  '../TestComponent'
-import ClassComponent from '../ClassComponent'
+// import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         
         <TestComponent name = "Cristiano Souza"/>
-        <ClassComponent name ="Teste"/>
+        {/* <ClassComponent name ="Teste"/> */}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from   'react'
+import React, { useState, useEffect } from   'react'
 import './TestComponent.css'
 
 function TestComponent (props: { name: string }) {
 
     const [age, setAge] = useState(21)
 
+    useEffect(() => {
+        console.log("Component has created")
+    }, [])
+
+    useEffect(() => {
+        console.log("Age has been created to: " + age)
+    }, [age])
+
+ 
    return  <div className="TestComponent"> Olá, {props.name}, {age} 
     <button onClick= {
         () => {
