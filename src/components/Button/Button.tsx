f1a650c611772c4d5dc74b28ad43e5a0ecd223bc
unknown <cristianoalmeida.souza@gmail.com>
2020-12-10 08:07:07 -0600

    add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Button.css'
+
+interface ButtonProps {
+    onClick?: () => void,
+    appendIcon: JSX.Element
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+    return <button className="AppButton" onClick={props.onClick}>
+        {props.children || 'nameless button'}
+        {props.appendIcon}
+    </button>
+}
+export default Button
