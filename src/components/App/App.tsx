ac2ab18b0e7673278f9be88a5e9810b689eab2db
unknown <cristianoalmeida.souza@gmail.com>
2020-12-10 09:01:29 -0600

    add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Header'
 
 import Button from '../../shared/Button'
 import Container from '../../shared/Container'
 import './App.css'
+import Input from '../../shared/Input'
 
 function TestComponent () {
   return <img src="https://t4.ftcdn.net/jpg/01/36/99/17/240_F_136991730_wMdWUuZB4cTsc5IabZatduaL0Fgcx5Lk.jpg" width="16px" height="16px" alt="search icon"/>
@@ -11,6 +12,9 @@ function TestComponent () {
 
 
 function App() {
+
+  const [street, setStreet] = useState('')
+
   return (
     <div className="App"> 
     <Header title="AlgaStock" />
@@ -21,6 +25,8 @@ function App() {
         > 
           Potato
         </Button>
+
+        <Input label="Street" placeholder="15h" value={street} onChange={e => setStreet(e.target.value)}/>
 
       </Container>
 
--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.css'
 
-interface ButtonProps {
+declare interface ButtonProps {
     onClick?: () => void,
     appendIcon: JSX.Element
 }
