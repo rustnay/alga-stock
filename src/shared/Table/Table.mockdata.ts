4c86fbd4e4be9ff8c4f9c6249c15ffd8bcbd2a07
unknown <cristianoalmeida.souza@gmail.com>
2020-12-10 12:24:00 -0600

    table Mock data

---
+++ src/shared/Table/Table.mockdata.ts
@@ -0,0 +1,43 @@
+import { deflate } from "zlib"
+
+export interface Product {
+    id: number
+    name: string
+    price: number
+    stock: number
+}
+
+const Products: Product[] = [
+    {
+        id: 1,
+        name: 'Cookie',
+        price: 1.25,
+        stock: 20
+    },
+    {
+        id: 2,
+        name: 'Milk 1L',
+        price: 2.99,
+        stock: 10
+    },
+    {
+        id: 3,
+        name: 'Detergent',
+        price: 0.75,
+        stock: 75
+    },
+    {
+        id: 4,
+        name: 'Water 1L',
+        price: 1.22,
+        stock: 100
+    },
+    {
+        id: 5,
+        name: 'Chocolate',
+        price: 4.65,
+        stock: 50
+    }
+]
+
+export default Products
